aea8ba0e6a4a22e46da173cec949be59c66ea4f4
Amayuru1999 <amarasingheau@gmail.com>
2024-07-17 23:48:25 +0530

    chore: Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* Your header content goes here */}
+        </div>
+    );
+};
+
+export default Header;
