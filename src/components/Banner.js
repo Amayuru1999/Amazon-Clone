d3ced97c843fc072cefe93dc6d18935512794763
Amayuru1999 <amarasingheau@gmail.com>
2024-07-18 22:19:49 +0530

    chore: Add Carousel to Banner component for image display

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,12 @@ import { Carousel } from 'react-responsive-carousel'
 const Banner = () => {
   return (
     <div className='relative'>
+        <Carousel
+            autoPlay
+            infiniteLoop
+        >
+
+        </Carousel>
       
     </div>
   )
