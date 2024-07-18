afef640b7d62d20fb952acc8033ff7f78aace38d
Amayuru1999 <amarasingheau@gmail.com>
2024-07-18 22:09:15 +0530

    chore: Add Banner component to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -8,6 +9,12 @@ export default function Home() {
         <title>Amazon Sri Lanka</title>
       </Head>
       <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner/>
+
+        {/* ProductFeed */}
+      </main>
     </div>
   );
 }
