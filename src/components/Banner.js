9d8c76f665056a1fbf98714d7d6567f68ad76dee
Amayuru1999 <amarasingheau@gmail.com>
2024-07-18 22:28:39 +0530

    feat: Add lazy loading for images in Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,6 +12,15 @@ const Banner = () => {
             showThumbs={false}
             interval={5000}
         >
+            <div>
+                <img loading='lazy' src='https://links.papareact.com/gi1' alt='' />
+            </div>
+            <div>
+                <img loading='lazy' src='https://links.papareact.com/6ff' alt='' />
+            </div>
+            <div>
+                <img loading='lazy' src='https://links.papareact.com/7ma' alt='' />
+            </div>
 
         </Carousel>
       
