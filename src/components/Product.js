ca65529a958f40bc054830879d9bcc86075320a7
Amayuru1999 <amarasingheau@gmail.com>
2024-07-19 19:22:34 +0530

    feat: Add star rating display to Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,6 +9,11 @@ const Product = ({id,title,price,description,category,image}) => {
       <p>{category}</p>
       <Image src={image} height={200} width={200} objectFit='contain' />
       <h4>{title}</h4>
+      <div className='flex'>
+        {Array(rating).fill().map((_,i)=>(
+          <StarIcon key={i} className='h-5 text-yellow-500' />
+        ))}
+        </div>
     </div>
   )
 }
