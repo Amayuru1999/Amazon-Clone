cde03444bc8d734c190a94c6b682c839a26e1f4a
Amayuru1999 <amarasingheau@gmail.com>
2024-07-19 22:43:53 +0530

    refactor: Improve ProductFeed layout with responsive grid and lazy loading for images

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,7 +4,9 @@ import Product from './Product'
 const ProductFeed = ({ products }) => {
   return (
     <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-      {products.map(({ id, title, price, description, category, image }) => (
+      {products
+        .slice(0,4)
+        .map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
           id={id}
@@ -16,7 +18,9 @@ const ProductFeed = ({ products }) => {
         />
     
     ))}
+    <img className='md:col-span-full' src='https://links.papareact.com/dyz' alt='' />
     </div>
+    
   )
 }
 
