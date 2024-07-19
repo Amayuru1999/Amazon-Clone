8acdfa0f9da6e1613fbd49156ef666a65d802378
Amayuru1999 <amarasingheau@gmail.com>
2024-07-19 21:54:48 +0530

    refactor: Improve Product component layout, add lazy loading for images, and display star ratings

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,45 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
-import { StarIcon } from '@heroicons/react/solid'
-import Currency from 'react-currency-formatter'
-import Prime from './../assets/prime.png'
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
+import Prime from "./../assets/prime.png";
 
-const MAX_RATING = 5
-const MIN_RATING = 1
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
 const Product = ({ id, title, price, description, category, image }) => {
   const [rating, setRating] = useState(0);
   const [hasPrime, setHasPrime] = useState(false);
 
   useEffect(() => {
-    setRating(Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING);
+    setRating(
+      Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+    );
     setHasPrime(Math.random() < 0.5);
   }, []);
 
   return (
-    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-      <p>{category}</p>
-      <Image src={image} height={200} width={200} objectFit='contain' />
-      <h4>{title}</h4>
-      <div className='flex'>
-        {Array(rating).fill().map((_, i) => (
-          <StarIcon key={i} className='h-5 text-yellow-500' />
-        ))}
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4 className="my-3">{title}</h4>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
       </div>
       <p>{description}</p>
       <div>
-        <Currency quantity={price} currency='LKR' />
+        <Currency quantity={price} currency="LKR" />
       </div>
       {hasPrime && (
         <div>
-          <Image src={Prime} alt='prime' className='w-12' />
+          <Image src={Prime} alt="prime" className="w-12" />
           <p>FREE Next-day Delivery</p>
         </div>
       )}
 
       <button>Add to Basket</button>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
