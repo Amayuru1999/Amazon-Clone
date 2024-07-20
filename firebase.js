e0e41d9aeb5399824c7505559e80fd12bffcb3de
Amayuru1999 <amarasingheau@gmail.com>
2024-07-20 08:13:40 +0530

    refactor: Update firebase configuration with new credentials

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNtVYrJReSByqR7fk3L6nIZjYk2ydKfSs",
+    authDomain: "amzn-clone-4e55f.firebaseapp.com",
+    projectId: "amzn-clone-4e55f",
+    storageBucket: "amzn-clone-4e55f.appspot.com",
+    messagingSenderId: "114568654516",
+    appId: "1:114568654516:web:4f1b0d0b3ad196bcc9f1e1"
+  };
