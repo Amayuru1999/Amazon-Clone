c91fa834fe5cea7eb0a03acb57efee092549653b
Amayuru1999 <amarasingheau@gmail.com>
2024-07-21 21:56:09 +0530

    refactor: Update checkout page to include a styled container for the main content area

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -11,7 +11,7 @@ function Checkout() {
         <div className='flex-grow m-5 shadow-sm'>
             <Image src="https://links.papareact.com/ikj" width={1020} height={250} objectFit="contain" />
             <div className='flex flex-col p-5 space-y-10 bg-white'>
-                <h1 className='text-3xl'>Your Shopping Basket</h1>
+                <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
             </div>
         </div>
         {/* Right */}
