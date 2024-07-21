92e4549906058145eb270dd56b9ef795021aadde
Amayuru1999 <amarasingheau@gmail.com>
2024-07-21 21:27:49 +0530

    refactor: Rename checkout function to Checkout component

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function checkout() {
+function Checkout() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function checkout() {
   )
 }
 
-export default checkout
+export default Checkout
