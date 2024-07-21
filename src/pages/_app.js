66b5754a92f35a96ed1d605310fdf6f61262805e
Amayuru1999 <amarasingheau@gmail.com>
2024-07-21 13:18:07 +0530

    refactor: Update _app.js to use ReduxProvider for state management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,16 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
-import { Provider as AuthProvider } from 'next-auth/react'
+import { Provider as ReduxProvider } from 'react-redux';
+import { Provider as AuthProvider } from 'next-auth/react';
+import { store } from '../app/store';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <AuthProvider session={pageProps.session}>
-      <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+      <ReduxProvider store={store}>
+        <Component {...pageProps} />
+      </ReduxProvider>
     </AuthProvider>
-    
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
