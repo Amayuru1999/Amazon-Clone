2c70ed2160edcdae313b39959cc4f6b5c8b18fb1
Amayuru1999 <amarasingheau@gmail.com>
2024-07-21 21:30:55 +0530

    refactor: Update checkout page to include Header component

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Checkout() {
   return (
-    <div>
-      
+    <div className='bg-gray-100'>
+      <Header />
     </div>
   )
 }
