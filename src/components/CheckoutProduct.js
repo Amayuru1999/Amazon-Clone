5205e7f42c8a2c02747c040b2a2283f793a49dbc
Amayuru1999 <amarasingheau@gmail.com>
2024-07-22 23:58:13 +0530

    refactor: Update CheckoutProduct component to include removeItemFromBasket functionality

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -30,6 +30,10 @@ function CheckoutProduct({
     };
     dispatch(addToBasket(product));
   };
+
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }));
+  }
   return (
     <div className="grid grid-cols-5">
       <Image src={image} height={200} width={200} objectFit="contain" />
@@ -55,7 +59,7 @@ function CheckoutProduct({
       {/* Right add/remove buttons */}
       <div className="flex flex-col space-y-2 my-auto justify-self-end">
         <button className="button mt-auto" onClick={addItemToBasket}>Add to Basket</button>
-        <button className="button mt-auto">Remove from Basket</button>
+        <button className="button mt-auto" onClick={removeItemFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
