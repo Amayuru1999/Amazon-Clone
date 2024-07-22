79de664ecff0c43f2c5e5eee115cbf4cfffd27ae
Amayuru1999 <amarasingheau@gmail.com>
2024-07-22 19:55:08 +0530

    refactor: Update CheckoutProduct component to include additional props for displaying product details in the checkout page

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-function CheckoutProduct() {
+function CheckoutProduct({
+    id,
+    title,
+    price,
+    rating,
+    description,
+    category,
+    image,
+    hasPrime,
+}) {
   return (
     <div>
       
