9294bf88b1dcee24730186b64d395cf922950486
Amayuru1999 <amarasingheau@gmail.com>
2024-07-22 19:47:44 +0530

    refactor: Update checkout page to include dynamic "Shopping Basket" heading

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
+import { selectItems } from '../slices/basketSlice'
 
 function Checkout() {
+  const items=useSelector(selectItems)
   return (
     <div className='bg-gray-100'>
       <Header />
@@ -11,8 +14,15 @@ function Checkout() {
         <div className='flex-grow m-5 shadow-sm'>
             <Image src="https://links.papareact.com/ikj" width={1020} height={250} objectFit="contain" />
             <div className='flex flex-col p-5 space-y-10 bg-white'>
-                <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+                <h1 className='text-3xl border-b pb-4'>
+                  {items.length === 0
+                    ?"Your Amazon Basket is empty."
+                    :"Shopping Basket"
+
+                  }
+                </h1>
             </div>
+
         </div>
         {/* Right */}
         <div></div>
