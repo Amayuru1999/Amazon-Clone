d630b85b885c01ddb10e9aa8d6d99d4a084c6c85
Amayuru1999 <amarasingheau@gmail.com>
2024-07-25 21:21:26 +0530

    feat:Added transform function to convert data into stripe accepting format

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -3,6 +3,13 @@ const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async (req,res) => {
     const {items,email}=req.body;
 
-    console.log(items);
-    console.log(email);
+    const transformedItems=items.map(item => ({
+        price_data:{
+            currency:'LKR',
+            product_data:{
+                name:item.title,
+                images:[item.image],
+            },
+        }
+    }));
 }
