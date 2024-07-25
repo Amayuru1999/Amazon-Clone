a1b82af651022e1e0ab7e0704819ac34747e4434
Amayuru1999 <amarasingheau@gmail.com>
2024-07-25 23:19:12 +0530

    feat:Added create-checkout-session function shipping rates

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -18,6 +18,7 @@ export default async (req,res) => {
     
     const session=await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
+        shipping_rates:['shr_1PgVf9HXdyJ63GGlieOCgg6H'],
         shipping_address_collection:{
           allowed_countries: ['US','CA','LK'],
         },
