43571a69b1865e8cf180842f93a8edb6ce406627
Amayuru1999 <amarasingheau@gmail.com>
2024-07-25 23:26:59 +0530

    feat:Added create-checkout-session redirection in frontend

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -23,6 +23,14 @@ function Checkout() {
           email: session.user.email,
       })
 
+      //Redirect user/customer to Stripe Checkout
+      const result = await stripe.redirectToCheckout({
+          sessionId: checkoutSession.data.id,
+      })
+      if(result.error){
+          alert(result.error.message);
+      }
+
   }
 
 
