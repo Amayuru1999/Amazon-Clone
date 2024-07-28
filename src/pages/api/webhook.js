43518e53210aea47655d3bf5ff1c9bde8fe96fb1
Amayuru1999 <amarasingheau@gmail.com>
2024-07-28 12:23:06 +0530

    feat:Secure a connection to Firebase

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,18 +1,16 @@
 import {buffer} from 'micro'
 import * as admin from 'firebase-admin'
 
+//Secure a connection to FIREBASE from the backend
+const serviceAccount= require('../../../permissions.json')
+const app = !admin.apps.length ? admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+}) : admin.app();
+
+
 export default async (req, res) => {
     if(req.method === 'POST'){
-        const buf = await buffer(req)
-        const payload = buf.toString()
-        const data = JSON.parse(payload)
 
-        await admin.firestore().collection('users').doc(data.id).set({
-            name: data.name,
-            email: data.email,
-            image: data.image
-        })
 
-        res.status(201).json({status: 'Success'})
     }
 }
