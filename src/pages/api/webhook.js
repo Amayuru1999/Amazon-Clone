21b2539534a3de62e89ddf630c69a8c43f9379e4
Amayuru1999 <amarasingheau@gmail.com>
2024-07-28 12:28:21 +0530

    feat:Implementing webhooks to connect stripe

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -7,10 +7,13 @@ const app = !admin.apps.length ? admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 }) : admin.app();
 
+//Establish connection to STRIPE
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
 export default async (req, res) => {
     if(req.method === 'POST'){
-
-
+        const  requestBuffer = await buffer(req);
     }
 }
