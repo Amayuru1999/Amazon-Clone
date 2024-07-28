529a9e11a9fce9e0590b11a7af5c0279e8f88724
Amayuru1999 <amarasingheau@gmail.com>
2024-07-28 12:52:05 +0530

    feat:Added fulfill order functionality #1

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -13,6 +13,23 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
 const fulfillOrder = async (session) => {
+    // console.log('Fulfilling order', session);
+    return app
+    .firestore()
+    .collection('users')
+    .doc(session.metadata.email)
+    .collection('orders')
+    .doc(session.id)
+    .set({
+        amount: session.amount_total / 100,
+        amount_shipping: session.total_details.amount_shipping / 100,
+        images: JSON.parse(session.metadata.images),
+        timestamp: admin.firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+        console.log(`SUCCESS: Order ${session.id} has been added to the database`);
+    });
+
 
 }
 
